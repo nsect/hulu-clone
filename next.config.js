327e406ac4aa291eb886b114a750a5f4c447e77b
nsect <nsecttheory@gmail.com>
2022-09-24 05:52:44 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  images: {
+    domains: ['links.papareact.com', 'image.tmdb.org'],
+  },
+};
